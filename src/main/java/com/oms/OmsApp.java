4d158e72cd9ba1119859f860ed48e9705a777f78
Ashutosh7796 <132451174+Ashutosh7796@users.noreply.github.com>
2023-12-29 14:48:54 +0530

    ADDED: Entity Classes with Mappings

---
+++ src/main/java/com/oms/OmsApp.java
@@ -0,0 +1,15 @@
+package com.oms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class OmsApp {
+
+    public static void main(final String[] args) {
+
+        SpringApplication.run(OmsApp.class, args);
+    }
+
+}
